77bc2efc75ceb15e8d3bd516454b5e76dcf7eb7e
Dipra <dipradas5940@gmail.com>
2024-07-11 23:03:47 +0600

    Add product completed

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -79,6 +79,70 @@ const Product = () => {
                                         <button className="btn btn-xs bg-red-500 text-white tracking-wider">DELETE</button>
                                     </td>
                                 </tr>
+                                <tr>
+                                    <th>2</th>
+                                    <td>My tree plant</td>
+                                    <td>Dolar ipsum dolar ipsum folar ipsum Dolar ipsum dolar ipsum folar ipsum </td>
+                                    <td>300</td>
+                                    <td className='flex items-center gap-2'>
+                                        <div className='w-[10px] h-[10px] rounded-full bg-green-400'>
+                                        </div>
+                                        <p>20</p>
+                                    </td>
+                                    <td>Bonsai</td>
+                                    <td className='flex justify-end gap-2'>
+                                        <button className="btn btn-xs bg-violet-400 text-white tracking-wider">EDIT</button>
+                                        <button className="btn btn-xs bg-red-500 text-white tracking-wider">DELETE</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>3</th>
+                                    <td>My tree plant</td>
+                                    <td>Dolar ipsum dolar ipsum folar ipsum Dolar ipsum dolar ipsum folar ipsum </td>
+                                    <td>300</td>
+                                    <td className='flex items-center gap-2'>
+                                        <div className='w-[10px] h-[10px] rounded-full bg-green-400'>
+                                        </div>
+                                        <p>20</p>
+                                    </td>
+                                    <td>Bonsai</td>
+                                    <td className='flex justify-end gap-2'>
+                                        <button className="btn btn-xs bg-violet-400 text-white tracking-wider">EDIT</button>
+                                        <button className="btn btn-xs bg-red-500 text-white tracking-wider">DELETE</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>4</th>
+                                    <td>My tree plant</td>
+                                    <td>Dolar ipsum dolar ipsum folar ipsum Dolar ipsum dolar ipsum folar ipsum </td>
+                                    <td>300</td>
+                                    <td className='flex items-center gap-2'>
+                                        <div className='w-[10px] h-[10px] rounded-full bg-green-400'>
+                                        </div>
+                                        <p>20</p>
+                                    </td>
+                                    <td>Bonsai</td>
+                                    <td className='flex justify-end gap-2'>
+                                        <button className="btn btn-xs bg-violet-400 text-white tracking-wider">EDIT</button>
+                                        <button className="btn btn-xs bg-red-500 text-white tracking-wider">DELETE</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>5</th>
+                                    <td>My tree plant</td>
+                                    <td>Dolar ipsum dolar ipsum folar ipsum Dolar ipsum dolar ipsum folar ipsum </td>
+                                    <td>300</td>
+                                    <td className='flex items-center gap-2'>
+                                        <div className='w-[10px] h-[10px] rounded-full bg-green-400'>
+                                        </div>
+                                        <p>20</p>
+                                    </td>
+                                    <td>Bonsai</td>
+                                    <td className='flex justify-end gap-2'>
+                                        <button className="btn btn-xs bg-violet-400 text-white tracking-wider">EDIT</button>
+                                        <button className="btn btn-xs bg-red-500 text-white tracking-wider">DELETE</button>
+                                    </td>
+                                </tr>
                             </tbody>
                         </table>
                     </div>
@@ -90,7 +154,7 @@ const Product = () => {
                 style={customStyles}
                 contentLabel="Add Product Modal"
             >
-                <div className='flex justify-between items-center mb-4'>
+                <div className='flex justify-between items-center mb-4 px-4'>
                     <h2
                         className='text-2xl tracking-wider'
                         ref={(_subtitle) => (subtitle = _subtitle)}>
@@ -100,7 +164,7 @@ const Product = () => {
                         <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>
                 </div>
-                <form onSubmit={onSubmit}>
+                <form onSubmit={onSubmit} className='px-4'>
                     <div className='grid grid-cols-2 gap-8'>
                         <div className='flex flex-col'>
                             <label className='mb-2 tracking-wider'>Title</label>
@@ -110,7 +174,7 @@ const Product = () => {
                                 className="input input-bordered w-full tracking-wider"
                                 {...register("title", { required: "Title is required" })}
                             />
-                            {errors.title && <p>{String(errors.title.message)}</p>}
+                            {errors.title && <p className="text-red-500">{String(errors.title.message)}</p>}
                         </div>
                         <div className='flex flex-col'>
                             <label className='mb-2 tracking-wider'>Description</label>
@@ -120,7 +184,7 @@ const Product = () => {
                                 className="input input-bordered w-full tracking-wider"
                                 {...register("description", { required: "Description is required" })}
                             />
-                            {errors.title && <p>{String(errors.title.message)}</p>}
+                            {errors.title && <p className="text-red-500">{String(errors.title.message)}</p>}
                         </div>
                     </div>
                     <div className='flex flex-col mt-4'>
@@ -133,36 +197,57 @@ const Product = () => {
                         />
                         {errors.briefDescription && <p className="text-red-500">{String(errors.briefDescription.message)}</p>}
                     </div>
-                    <div>
-                        <label>Title</label>
-                        <input {...register("title", { required: "Title is required" })} />
-                        {errors.title && <p>{String(errors.title.message)}</p>}
+                    <div className='grid grid-cols-3 gap-8 mt-4'>
+                        <div className='flex flex-col'>
+                            <label className='mb-2 tracking-wider'>Category</label>
+                            <select
+                                className="select select-bordered w-full max-w-xs"
+                                {...register("category", { required: "Category is required" })}
+                            >
+                                <option value="" disabled selected>Select a category</option>
+                                <option value="Han Solo">Han Solo</option>
+                                <option value="Greedo">Greedo</option>
+                            </select>
+                            {errors.category && <p className="text-red-500">{String(errors.category.message)}</p>}
+                        </div>
+                        <div className='flex flex-col'>
+                            <label className='mb-2 tracking-wider'>Price</label>
+                            <input
+                                type="number"
+                                placeholder="Enter Price here"
+                                className="input input-bordered w-full tracking-wider"
+                                {...register("price", { required: "Price is required", min: { value: 0, message: "Price cannot be negative" } })}
+                            />
+                            {errors.price && <p className="text-red-500">{String(errors.price.message)}</p>}
+                        </div>
+                        <div className='flex flex-col'>
+                            <label className='mb-2 tracking-wider'>Stock</label>
+                            <input
+                                type="number"
+                                placeholder="Enter Stock here"
+                                className="input input-bordered w-full tracking-wider"
+                                {...register("stock", { required: "Stock is required", min: { value: 0, message: "Stock cannot be negative" } })}
+                            />
+                            {errors.stock && <p className="text-red-500">{String(errors.stock.message)}</p>}
+                        </div>
                     </div>
-                    <div>
-                        <label>Description</label>
-                        <input {...register("description", { required: "Description is required" })} />
-                        {errors.description && <p>{String(errors.description.message)}</p>}
+                    <div className='grid grid-cols-12 gap-8 mt-4 items-center'>
+                        <div className='flex flex-col col-span-8'>
+                            <label className='mb-2 tracking-wider'>Product Image</label>
+                            <input type="file" className="file-input file-input-bordered w-full" />
+                        </div>
+                        <div className='col-span-4'>
+                            <div className=" cursor-pointer mt-9 flex items-center gap-3">
+                                <input type="checkbox" className="checkbox" />
+                                <p className="text">Featured Product</p>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <label>Stock</label>
-                        <input type="number" {...register("stock", { required: "Stock is required", min: 0 })} />
-                        {errors.stock && <p>{String(errors.stock.message)}</p>}
-                    </div>
-                    <div>
-                        <label>Rating</label>
-                        <input type="number" step="0.1" {...register("rating", { required: "Rating is required", min: 0, max: 5 })} />
-                        {errors.rating && <p>{String(errors.rating.message)}</p>}
-                    </div>
-                    <div>
-                        <label>Category</label>
-                        <input {...register("category", { required: "Category is required" })} />
-                        {errors.category && <p>{String(errors.category.message)}</p>}
-                    </div>
-                    <div>
-                        <label>Featured</label>
-                        <input type="checkbox" {...register("featured")} />
-                    </div>
-                    <button type="submit">Add Product</button>
+                    <button
+                        className='bg-[#66a15b] rounded-xl py-3 px-10 text-white mt-8 mb-5'
+                        type="submit">
+                        ADD PRODUCT
+                    </button>
                 </form>
             </Modal>
         </div>
