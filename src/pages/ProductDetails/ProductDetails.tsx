ce9d66902ab3fe1f059d51282435b8c0362bc22a
Dipra <dipradas5940@gmail.com>
2024-07-13 00:48:00 +0600

    Details responsive

--- src/pages/ProductDetails/ProductDetails.tsx
+++ src/pages/ProductDetails/ProductDetails.tsx
@@ -5,11 +5,11 @@ const ProductDetails = () => {
     return (
         <div>
             <div className="container mx-auto mt-20 mb-20">
-                <div className="px-32 grid grid-cols-2 gap-20 items-center">
-                    <div className="border shadow-md">
+                <div className="sm:px-12 2xl:px-32 grid grid-cols-1 lg:grid-cols-2 gap-20 items-center">
+                    <div className="border shadow-md w-4/5 mx-auto">
                         <img src={p4} alt="" />
                     </div>
-                    <div>
+                    <div className='px-10'>
                         <p className='text-4xl font-semibold mb-5'>
                             Alocasia Chantrieri
                         </p>
@@ -39,7 +39,7 @@ const ProductDetails = () => {
                             </p>
                         </div>
                         <div className='flex gap-3 items-center mb-5'>
-                            <p className='text-gray-500'>
+                            <p className='text-gray-500 min-w-10'>
                                 Tags :
                             </p>
                             <div className='flex gap-2'>
@@ -59,7 +59,7 @@ const ProductDetails = () => {
                         </div>
                     </div>
                 </div>
-                <div className='px-32 mt-20'>
+                <div className='px-10 sm:px-12 2xl:px-32 mt-20'>
                     <p className='text-xl tracking-wider pb-2 border-b-2 border-[#66a15b] inline font-medium'>Description</p>
                     <p className='mt-7 text-gray-500 text-base tracking-wider font-normal leading-9'>
                         Lorem ipsum dolor sit amet consectetur adipiscing elit scelerisque integer, quam dapibus per risus donec semper vulputate interdum, imperdiet mus rhoncus commodo ultricies class urna tincidunt. Imperdiet vitae lacus etiam metus ut nisl curae, conubia enim scelerisque quis facilisis torquent, ultricies orci faucibus dictumst mauris curabitur. Massa risus nec sociosqu fames montes accumsan iaculis justo turpis luctusThis generator uses a dictionary of Latin words to construct passages of Lorem Ipsum text that meet your desired length. The sentence and paragraph durations and punctuation dispersal are calculated using Gaussian distribution, based on statistical analysis of real world texts. This ensures that the generated Lorem Ipsum text is unique, free of repetition and also resembles readable text as much as possible.z
