813d65f08232f41c2425a2b6799a8f3d4503c404
Dipra <dipradas5940@gmail.com>
2024-07-13 15:32:55 +0600

    Featured ts issue fixed

--- src/pages/Home/FeaturedProduct/FeaturedProduct.tsx
+++ src/pages/Home/FeaturedProduct/FeaturedProduct.tsx
@@ -1,7 +1,22 @@
 import plant3 from '../../../assets/images/product/plant-3.png';
+import { useGetProductQuery } from '../../../redux/features/product/productApi';
 import FeaturedProductCard from './FeaturedProductCard';
 
+interface Product {
+    id: string;
+    title: string;
+    price: number;
+    featured: boolean;
+    image: string;
+    rating: number
+}
+
 const FeaturedProduct = () => {
+
+    const { data } = useGetProductQuery(undefined);
+    const productData = data?.data;
+    const featuredProducts = productData?.filter((product: { featured: boolean; }) => product.featured);
+
     return (
         <div className="mb-24">
             < div className="container mx-auto" >
@@ -9,11 +24,18 @@ const FeaturedProduct = () => {
                     <p className="text-center font-semibold tracking-widest text-black text-3xl mb-5">FEATURE PRODUCTS</p>
                     <img className='mx-auto mt-3' src={plant3} alt="" />
                     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-center lg:grid-cols-5 gap-4 mt-16 mb-32'>
-                        <FeaturedProductCard />
-                        <FeaturedProductCard />
-                        <FeaturedProductCard />
-                        <FeaturedProductCard />
-                        <FeaturedProductCard />
+                        {
+                            featuredProducts?.length &&
+                            featuredProducts.map(
+                                (
+                                    product: Product) => (
+                                    <FeaturedProductCard
+                                        key={product.id}
+                                        product={product}
+                                    />
+                                )
+                            )
+                        }
                     </div>
                 </div>
             </ div>
--- src/pages/Home/FeaturedProduct/FeaturedProductCard.tsx
+++ src/pages/Home/FeaturedProduct/FeaturedProductCard.tsx
@@ -1,12 +1,39 @@
-import plant4 from '../../../assets/images/product/plant-4.jpg';
 import OutlineButton from '../../../components/button/outlineButton/OutlineButton';
 
-const FeaturedProductCard = () => {
+interface Product {
+    image: string;
+    title: string;
+    price: number;
+    featured: boolean;
+    rating: number;
+}
+
+interface FeaturedProductCardProps {
+    product: Product;
+}
+
+const FeaturedProductCard = ({ product }: FeaturedProductCardProps) => {
+
+    const renderStars = (rating: number, color: string) => {
+        const stars = [];
+        for (let i = 1; i <= 5; i++) {
+            if (i <= rating) {
+                stars.push(<span key={i} style={{ color }}>&#9733;</span>);
+            } else {
+                stars.push(<span key={i} style={{ color }}>&#9734;</span>);
+            }
+        }
+        return stars;
+    };
+
     return (
         <div className='text-center'>
-            <img className='w-80 mx-auto' src={plant4} alt="" />
-            <p className='font-semibold mb-1'>Biotech Garden Jade Plant</p>
-            <p className='font-semibold text-xl mb-4' >$22.00</p>
+            <img className='w-80 mx-auto h-56' src={product.image} alt="" />
+            <p className='font-semibold mb-1'>{product.title}</p>
+            <p className='font-semibold text-xl mb-1'>$ {product.price}</p>
+            <div className='mb-4'>
+                {renderStars(product.rating, '#66a15b')} {/* Example: Use 'gold' as the color */}
+            </div>
             <div>
                 <OutlineButton title='ADD TO CART' />
             </div>
