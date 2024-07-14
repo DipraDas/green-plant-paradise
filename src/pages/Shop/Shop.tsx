30d360b989ee3db15ab52fc06e191e241ab6800c
Dipra <dipradas5940@gmail.com>
2024-07-14 22:22:37 +0600

    Filtering completed in shop page

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -4,6 +4,7 @@ import ProductCard from './ProductCard/ProductCard';
 import { useGetCategoryQuery } from '../../redux/features/category/categoryApi';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { filterByCategory } from '../../redux/features/filter/filterSlice';
+import { useEffect, useState } from 'react';
 
 interface Category {
     name: string;
@@ -14,19 +15,23 @@ interface Product {
     price: number;
     image: string;
     rating: number;
-    categories: Category[]
+    categories: Category[];
+}
+
+interface SelectedCategory {
+    category: string;
 }
 
 const Shop = () => {
     const dispatch = useAppDispatch();
+    const [sortBy, setSortBy] = useState('');
 
-    const selectedCategory = useAppSelector(state => state.filter.filterByCategory);
+    const selectedCategory: SelectedCategory = useAppSelector(state => state.filter.filterByCategory);
 
     const { data: productDataResponse } = useGetProductQuery(undefined);
-    const productData = productDataResponse?.data;
+    const productData = productDataResponse?.data || [];
 
     const { data: categoryDataResponse } = useGetCategoryQuery(undefined);
-
     let categoryData = [];
     if (categoryDataResponse && categoryDataResponse.success) {
         categoryData = categoryDataResponse.data;
@@ -36,9 +41,32 @@ const Shop = () => {
         dispatch(filterByCategory({ category }));
     };
 
-    const filteredProducts = selectedCategory
-        ? productData?.filter((product: Product) => product.categories[0].name === selectedCategory.category)
-        : productData;
+    const getFilteredAndSortedProducts = () => {
+        let filteredProducts = selectedCategory.category
+            ? selectedCategory.category === 'All'
+                ? productData
+                : productData.filter((product: Product) => product.categories.some(cat => cat.name === selectedCategory.category))
+            : productData;
+
+        switch (sortBy) {
+            case 'rated':
+                return filteredProducts.slice().sort((a: Product, b: Product) => b.rating - a.rating);
+            case 'highPrice':
+                return filteredProducts.slice().sort((a: Product, b: Product) => b.price - a.price);
+            case 'lowPrice':
+                return filteredProducts.slice().sort((a: Product, b: Product) => a.price - b.price);
+            case 'latest':
+                return filteredProducts.slice().sort((a: Product, b: Product) => parseInt(b._id.substring(0, 8), 16) - parseInt(a._id.substring(0, 8), 16));
+            default:
+                return filteredProducts;
+        }
+    };
+
+    const filteredProducts = getFilteredAndSortedProducts();
+
+    useEffect(() => {
+        getFilteredAndSortedProducts();
+    }, [sortBy, selectedCategory]);
 
     return (
         <div>
@@ -66,6 +94,16 @@ const Shop = () => {
                         <div className='bg-gray-100 px-6 py-6 rounded-xl mb-6'>
                             <p className='text-2xl mb-3 tracking-wider'>Categories</p>
                             <div className='w-24 h-[2px] bg-gray-300 mb-8'></div>
+                            <div
+                                onClick={() => handleCategory('All')}
+                                className={`flex gap-1 my-4 items-center cursor-pointer ${selectedCategory.category === 'All' ? 'text-[#66a15b]' : 'text-gray-600'}`}
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-4">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                                </svg>
+                                <p className='text-md tracking-wider hover:text-[#66a15b]'>All</p>
+                            </div>
+                            <div className='w-full h-[1px] bg-gray-300'></div>
                             {
                                 categoryData?.map((category: { name: string }, index: number) => (
                                     <div key={index}>
@@ -90,24 +128,42 @@ const Shop = () => {
                             <p className='text-2xl mb-3 tracking-wider'>Sort By</p>
                             <div className='w-24 h-[2px] bg-gray-300 mb-8'></div>
                             <div className="grid grid-cols-2 gap-5">
-                                <p className='tracking-wider text-center bg-white rounded-md py-3'>Default</p>
-                                <p className='tracking-wider text-center bg-white rounded-md py-3'>Latest</p>
-                                <p className='tracking-wider text-center bg-white rounded-md py-3'>High Price</p>
-                                <p className='tracking-wider text-center bg-white rounded-md py-3'>Low Price</p>
-                                <p className='tracking-wider text-center bg-white rounded-md py-3'>Rated</p>
+                                <p
+                                    className={`tracking-wider text-center rounded-md py-3 cursor-pointer ${sortBy === 'latest' ? 'bg-[#66a15b] text-white' : 'bg-white'}`}
+                                    onClick={() => setSortBy('latest')}
+                                >
+                                    Latest
+                                </p>
+                                <p
+                                    className={`tracking-wider text-center rounded-md py-3 cursor-pointer ${sortBy === 'highPrice' ? 'bg-[#66a15b] text-white' : 'bg-white'}`}
+                                    onClick={() => setSortBy('highPrice')}
+                                >
+                                    High Price
+                                </p>
+                                <p
+                                    className={`tracking-wider text-center rounded-md py-3 cursor-pointer ${sortBy === 'lowPrice' ? 'bg-[#66a15b] text-white' : 'bg-white'}`}
+                                    onClick={() => setSortBy('lowPrice')}
+                                >
+                                    Low Price
+                                </p>
+                                <p
+                                    className={`tracking-wider text-center rounded-md py-3 cursor-pointer ${sortBy === 'rated' ? 'bg-[#66a15b] text-white' : 'bg-white'}`}
+                                    onClick={() => setSortBy('rated')}
+                                >
+                                    Rated
+                                </p>
                             </div>
                         </div>
                     </div>
                     <div className="col-span-12 lg:col-span-8 xl:col-span-9">
                         <div className='flex items-center gap-1 justify-center border w-56 mx-auto py-3 rounded-md'>
-                            <p className='font-bold text-[#66a15b]'>{productDataResponse?.length}</p>
                             <p className='font-bold text-[#66a15b]'>{filteredProducts?.length}</p>
                             <p className=''>Product Found of </p>
                             <p className='font-bold text-[#66a15b]'>{productData?.length}</p>
                         </div>
-                        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3 gap-4 mt-10 mb-10'>
+                        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
                             {
-                                filteredProducts?.map((product: Product) => (
+                                filteredProducts.map((product: Product) => (
                                     <ProductCard key={product._id} product={product} />
                                 ))
                             }
